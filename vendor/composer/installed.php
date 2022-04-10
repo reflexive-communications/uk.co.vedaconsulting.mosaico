b8cd5e007ea195d979300b44ce9755df0094a594
Sandor Semsey <sandor@es-progress.hu>
2022-04-10 22:22:17 +0200

    dependencies 2.10

---
+++ vendor/composer/installed.php
@@ -0,0 +1,62 @@
+<?php return array(
+    'root' => array(
+        'name' => 'civipkg/uk.co.vedaconsulting.mosaico',
+        'pretty_version' => '2.10',
+        'version' => '2.10.0.0',
+        'reference' => '441b226e7e8d7f67e82e22cb39692143f2492b58',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'civicrm/composer-downloads-plugin' => array(
+            'pretty_version' => 'v3.0.1',
+            'version' => '3.0.1.0',
+            'reference' => '3aabb6d259a86158d01829fc2c62a2afb9618877',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../civicrm/composer-downloads-plugin',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'civipkg/uk.co.vedaconsulting.mosaico' => array(
+            'pretty_version' => '2.10',
+            'version' => '2.10.0.0',
+            'reference' => '441b226e7e8d7f67e82e22cb39692143f2492b58',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'guzzlehttp/guzzle' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '^6.5',
+            ),
+        ),
+        'guzzlehttp/psr7' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '^1.8.5',
+            ),
+        ),
+        'intervention/image' => array(
+            'pretty_version' => '2.7.1',
+            'version' => '2.7.1.0',
+            'reference' => '744ebba495319501b873a4e48787759c72e3fb8c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../intervention/image',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'togos/gitignore' => array(
+            'pretty_version' => '1.1.1',
+            'version' => '1.1.1.0',
+            'reference' => '32bc0830e4123f670adcbf5ddda5bef362f4f4d4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../togos/gitignore',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
